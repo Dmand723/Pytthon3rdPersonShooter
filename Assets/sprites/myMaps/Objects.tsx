ab7883d499b1c0c415dfe310551a0dca508916c1
Dmand723 <dawson.simmons723@gmail.com>
2025-03-17 08:48:28 -0600

    Update map

--- Assets/sprites/myMaps/Objects.tsx
+++ Assets/sprites/myMaps/Objects.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Objects" tilewidth="800" tileheight="628" tilecount="21" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="Objects" tilewidth="800" tileheight="628" tilecount="22" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="3">
   <image width="64" height="56" source="../objects/rock_3.png"/>
@@ -63,5 +63,8 @@
  </tile>
  <tile id="26">
   <image width="24" height="24" source="../tilesets/trapClosed.png"/>
+ </tile>
+ <tile id="27">
+  <image width="134" height="128" source="../player/down_idle/0.png"/>
  </tile>
 </tileset>
